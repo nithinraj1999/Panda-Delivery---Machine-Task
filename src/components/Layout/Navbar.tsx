5f064ecbd9b665f77877fbaf757e8b45d137bac9
nithinraj1999 <vnithinraj99@gmail.com>
2025-09-21 13:57:54 +0530

    feat: update font to match Figma design

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -7,15 +7,14 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className=" hidden xl:flex justify-between z-50
- py-[40px] px-[80px] bg-white fixed shadow-[0_0_36px_0_rgba(0,0,0,0.06)] w-full h-[100px] max-width-[1440px] z-50">
+      <nav className=" hidden xl:flex justify-between z-50 py-[40px] px-[80px] bg-white fixed shadow-[0_0_36px_0_rgba(0,0,0,0.06)] w-full h-[100px] max-width-[1440px] z-50">
         <div className="flex items-center">
           <Logo />
         </div>
-        <ul className="flex items-center space-x-[40px] text-base h-[26px] gap-[8px] leading-[160%]">
-          <li><a href="#" className="text-[#F89938]  ">Home</a></li>
-          <li><a href="#" >Why Panda</a></li>
-          <li className="relative group">
+        <ul className="flex items-center space-x-[40px]  font-atypdisplay  h-[26px] gap-[8px] leading-[160%]">
+          <li><a href="#" className="text-[#F89938]   font-semibold">Home</a></li>
+          <li><a href="#" className="font-normal">Why Panda</a></li>
+          <li className="relative group font-normal ">
             <button className=" flex items-center">
               Services
               <svg className="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -27,16 +26,16 @@ const Navbar = () => {
               <a href="#" className="block px-4 py-2 hover:bg-gray-100">Service 2</a>
             </div>
           </li>
-          <li><a href="#" >Track Shipment</a></li>
-          <li><a href="#" >Gallery</a></li>
-          <li><a href="#" >Blog</a></li>
-          <li><a href="#" >Contact Us</a></li>
+          <li><a href="#" className="font-normal">Track Shipment</a></li>
+          <li><a href="#" className="font-normal">Gallery</a></li>
+          <li><a href="#" className="font-normal">Blog</a></li>
+          <li><a href="#" className="font-normal">Contact Us</a></li>
         </ul>
 
         <div className=" h-[44px]">
           <a
             href="#"
-            className="bg-[#F89938]  w-[132px] gap-[12px] text-white font-semibold py-2 px-6 rounded-[9999px]"
+            className="bg-[#F89938]  w-[132px] gap-[12px] text-white font-atypdisplay font-semibold py-2 px-6 rounded-[9999px]"
           >
             Client Portal
           </a>
