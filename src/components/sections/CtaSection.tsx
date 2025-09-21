31917bf57a5494e9d42fb41744f50fdc3d630592
nithinraj1999 <vnithinraj99@gmail.com>
2025-09-21 09:21:40 +0530

    fix(ctaSection): adjust alignment, spacing, and responsiveness

--- src/components/sections/CtaSection.tsx
+++ src/components/sections/CtaSection.tsx
@@ -2,19 +2,24 @@ import React from "react";
 
 const CtaSection: React.FC = () => {
     return (
-        <section className="bg-[#002855] text-white h-[445px] flex pl-[80px]">
-            <div className="flex-1 flex items-center justify-center">
-                <div>
-                    <h2 className="text-3xl lg:text-4xl font-bold mb-4">
-                        Let’s Simplify Your Logistics Journey with Us!
-                    </h2>
-                    <p className="text-lg mb-6">
-                        Experience smooth logistics management with our innovative services
-                    </p>
+        <section className="bg-[#002855] text-white h-[411px] flex pl-[80px]">
+            <div className="py-30 pr-[80px]">
+                <div className="flex-1 flex flex-col items-start justify-center  h-[153px] gap-4">
+                    <div>
+                        <h2 className="text-[36px] lg:text-4xl font-semibold mb-4 leading-[140%]">
+                            Let’s Simplify Your<br /> Logistics Journey with Us!
+                        </h2>
+                        <p className="text-lg mb-6">
+                            Experience smooth logistics management with our innovative services
+                        </p>
+                    </div>
+
                     <button className="bg-white text-black px-6 py-3 rounded-full font-medium shadow hover:bg-gray-200 transition">
                         Start 14-day Free Trial
                     </button>
                 </div>
+
+
             </div>
 
             <div className="flex-1 flex items-center justify-center bg-black">
