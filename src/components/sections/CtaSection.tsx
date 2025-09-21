f1bb35373a30da13559980c97f6e05ed4aaba8ca
nithinraj1999 <vnithinraj99@gmail.com>
2025-09-21 09:51:42 +0530

    chore: hide right column image on screens below lg

--- src/components/sections/CtaSection.tsx
+++ src/components/sections/CtaSection.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const CtaSection: React.FC = () => {
     return (
         <section className="bg-[#002855] text-white h-[411px] flex pl-[80px]">
-            <div className="py-30 pr-[80px]">
-                <div className="flex-1 flex flex-col items-start justify-center  h-[153px] gap-4">
+            <div className="flex-1 flex flex-col justify-center pr-[40px]">
+                <div className="flex flex-col items-start gap-4 h-[153px]">
                     <div>
                         <h2 className="text-[36px] lg:text-4xl font-semibold mb-4 leading-[140%]">
                             Let’s Simplify Your<br /> Logistics Journey with Us!
@@ -18,18 +18,15 @@ const CtaSection: React.FC = () => {
                         Start 14-day Free Trial
                     </button>
                 </div>
-
-
             </div>
 
-            <div className="flex-1 flex items-center justify-center bg-black">
+            <div className="flex-1 hidden lg:flex items-center justify-center bg-black">
                 <img
                     src="/images/container-cta.jpg"
                     alt="Right Content"
                     className="w-full h-full object-cover"
                 />
             </div>
-
         </section>
     );
 };
